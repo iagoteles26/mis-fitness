f66011879b30d1ac2b67fa6034ba35d6184029a6
iagoteles26 <teles.iago15@gmail.com>
2026-02-20 19:27:21 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -55,7 +55,7 @@ export default function Home() {
       <p>Total: R$ {total()}</p>
 
       <a
-        href="https://wa.me/5500000000000"
+        href="https://wa.me/5588997427649?text=Olá%20vim%20pelo%20site%20da%20loja%20MIS%20Fitness%20e%20quero%20finalizar%20meu%20pedido"
         target="_blank"
       >
         <button>
